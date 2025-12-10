9e8091f20767e0ea8b27f8aab0da8a38cc3d6200
campbellmbrown <campbellbrown093@gmail.com>
2025-12-10 20:57:03 +1300

    Introduce Pretty Python class and handle Python tuples

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,37 @@ mod pretty;
 
 use std::io::Write;
 
+use crate::pretty::{Config, print};
+
 use pyo3::prelude::*;
 
-#[pyfunction]
-fn pretty_print(obj: &Bound<'_, PyAny>) -> PyResult<()> {
-    let mut stdout = std::io::stdout();
-    pretty::print(obj, 0, &mut stdout)?;
-    writeln!(stdout)?;
-    Ok(())
+#[pyclass]
+pub struct Pretty {
+    config: Config,
+}
+
+#[pymethods]
+impl Pretty {
+    #[new]
+    fn new(indent: Option<usize>) -> Self {
+        Self {
+            config: Config {
+                indent: indent.unwrap_or(4),
+            },
+        }
+    }
+
+    fn print(&mut self, obj: &Bound<'_, PyAny>) -> PyResult<()> {
+        let mut stdout = std::io::stdout();
+        print(obj, &self.config, 0, &mut stdout)?;
+        writeln!(stdout)?;
+        Ok(())
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pyo3::pymodule]
 mod foliar {
     #[pymodule_export]
-    use super::pretty_print;
+    use super::Pretty;
 }
